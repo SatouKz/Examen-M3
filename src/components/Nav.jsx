9577ef18ea79e76aa0fbff8ccbd81db6a1ab5af2
SatouKz <alexander.ajalla404@gmail.com>
2022-03-02 20:52:26 -0300

    test/finish

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import Logo from '../logoHenry.png'
-import SearchBar from './SearchBar.jsx';
-import './Nav.css';
+import React from "react";
+import Logo from "../logoHenry.png";
+import SearchBar from "./SearchBar.jsx";
+import "./Nav.css";
 
-function Nav({onSearch}) {
+function Nav({ onSearch }) {
   return (
-    <div>Nav</div>
+    <nav class="navbar navbar-dark bg-dark">
+      <a class="navbar-brand" href="https://openweathermap.org/api">
+        <img
+          src={Logo}
+          width="30"
+          height="30"
+          class="d-inline-block align-top"
+          alt="img"
+        />
+        Henry - Weather App
+      </a>
+      <SearchBar onSearch={onSearch} />
+    </nav>
   );
-};
+}
 
 export default Nav;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-
-export default function SearchBar({onSearch}) {
+// import Card from "./Card";
+export default function SearchBar({ onSearch }) {
+  const [city, setCity] = useState("");
+  console.log(city);
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
-      onSearch("Cairns");
-    }}>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        onSearch(city);
+      }}
+      class="form-inline"
+    >
       <input
         type="text"
         placeholder="Ciudad..."
+        class="form-control mr-sm-2"
+        aria-label="Search"
+        onChange={(e) => setCity(e.target.value)}
       />
-      <input type="submit" value="Agregar" />
+      <input
+        type="submit"
+        value="Agregar"
+        class="btn btn-outline-success my-2 my-sm-0"
+      />
     </form>
   );
 }
